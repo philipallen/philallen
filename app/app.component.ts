20e03aab18a3d752957f070d70b92f95882ef0eb
Phil Allen <phil.allen@imsmaxims.com>
2016-11-09 12:53:38 +0000

    Small changes to see if it works

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,19 +3,19 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     styles: [`h1 {
-	color: white;
-	background: darkgray;
-	padding: 20px;
-}
-`],
+		color: white;
+		background: darkgray;
+		padding: 20px;
+	}
+	`],
     template: `
-<h1>My First {{name}} app</h1>
-<router-outlet></router-outlet>
+	<h1>I think this {{name}} do my CV website</h1>
+	<router-outlet></router-outlet>
 
-<a [routerLink]="['/']">Home</a> | <a [routerLink]="['/about/', { id: 2 }]">About</a>`,
+	<a [routerLink]="['/']">Home</a> | <a [routerLink]="['/about/', { id: 2 }]">About</a>`,
 })
 export class AppComponent {
-    name: string = "Angular 2 on Express";
+    name: string = "is how I will";
 
     constructor() {}
 }
